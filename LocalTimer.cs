110194949aeb7cd637ef75e41da73ded5cd264dd
Can Pehlivan <cpehlivan544@gmail.com>
2024-02-02 20:51:08 +0300

    first commit

---
+++ LocalTimer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Timers;
+
+namespace DateAndTime
+{
+    class LocalTimer
+    {
+        public TimeSpan IntervalTime { get; set; }
+        public Timer timer { get; set; }
+        
+        public DateTime DateTimeNow { get; set; }
+
+        private bool _active;
+        
+        private double intervals; //counts how many intervals have elapsed
+        
+        private double indicatorIntervals; //the number of intervals between indicator dots. This should not be confused with intervalIndicator.
+                                           //I did not choose that definition because I think it's functioning like how a loading screen does.
+        public bool active
+        {
+            get
+            {
+                return _active;
+            }
+
+            set
+            {
+                _active = value;
+                if (value)
+                {
+                    timer.Elapsed += Timer_Elapsed;
+                }
+                else
+                {
+                    timer.Elapsed -= Timer_Elapsed;
+                }
+            }
+        }
+        public LocalTimer(TimeSpan intervalTime, DateTime now, int indicatorIntervalsDelay = 5)
+        {
+            IntervalTime = intervalTime;
+
+            DateTimeNow = now;
+
+            timer = new Timer();
+            
+            intervals = timer.Interval;
+            
+            indicatorIntervals = indicatorIntervalsDelay;
+            
+            timer.Start();
+            
+            active = true;
+        }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            intervals++;
+
+            if (intervals == IntervalTime.Milliseconds)
+            {
+                // TODO: The timer is behind the real time in seconds.
+
+                timer.Stop();
+
+                Console.WriteLine("\nTime's up!");
+                
+                DateTimeNow += IntervalTime;
+                
+                Console.WriteLine($"It is now:{DateTimeNow}");
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Beep(432, 500);
+                }
+
+                Console.WriteLine("If you want to exit the program you can enter \x1b[1mexit\x1b[0m");
+
+                var command = Console.ReadLine();
+
+                if (command == "exit")
+                {
+                    Console.WriteLine("Bye!");
+
+                    Environment.Exit(0);
+                }
+
+            }
+
+            else
+            {
+                if (intervals % indicatorIntervals == 0.0) Console.Write(".");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to our basic date and time demo.");
+            
+            Console.WriteLine("It is currently: " + DateTime.Now + " local time.");
+            
+            Console.WriteLine("Let's set a timer.");
+
+            Console.WriteLine("When do you want to set the timer?");
+
+            Console.WriteLine("Please enter a date exactly like above.");
+
+            var date = Console.ReadLine();
+
+            try
+            {
+                if (DateTime.TryParse(date, out DateTime temp) == true)
+                {
+
+                    var now = DateTime.Now;
+
+                    TimeSpan interval = temp - now;
+
+                    LocalTimer timer = new LocalTimer(interval,now);
+
+                    Console.WriteLine("Timer started at: " + now + " local time.");
+
+                    Console.WriteLine("Timer set.Please wait to get notified.");
+
+                    Console.WriteLine("If you want to stop the timer you can enter \x1b[1mcancel\x1b[0m");
+
+                    var command = Console.ReadLine();
+
+                    if (command == "cancel")
+                    {
+                        Console.WriteLine("Bye!");
+                        timer.timer.Stop();
+                    }
+                }
+            }
+
+            catch (ArgumentException argEx)
+            {
+                Console.WriteLine("Probably you did not give an acceptable input. Please enter the date in true format.");
+                
+                Console.WriteLine("Formatting is should be exactly this:");
+                
+                Console.WriteLine("Day.Month.Year Hour:Minute:Second");
+                
+                Console.WriteLine($"Error message: {argEx.Message}");
+            }
+        }
+    }
+}
